ec9c5e8aa3c0afa4a5c8918e84408f4600706cee
Douglas <cbdouglasvirissimo@hotmail.com>
2023-04-17 06:53:01 -0300

    feat: all migrations completed

---
+++ src/migrations/20230417092818-create-posts-categories.js
@@ -0,0 +1,30 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('posts_categories', {
+      post_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'blog_posts',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+      category_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('post_categories');
+  },
+};
